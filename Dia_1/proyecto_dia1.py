a4d8c0e7d8135c6b0b3987a85ebd353203632883
GsusMR <jmoreno91@alumnos.uaq.mx>
2025-09-23 20:32:07 -0600

    reorganized project

---
+++ Dia_1/proyecto_dia1.py
@@ -0,0 +1,7 @@
+print("¿Cuál es tu color favorito en inglés?")
+color=input()
+
+print("¿Cuál es tu animal favorito en inglés?")
+animal=input()
+
+print("EL nombre de tu cerveza es:\n" + color + " " + animal)
