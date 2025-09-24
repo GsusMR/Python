0815f1d7412bd35aa87c23cc94af8d14ca076331
GsusMR <jmoreno91@alumnos.uaq.mx>
2025-09-23 20:05:04 -0600

    Initial commit

---
+++ primer_proyecto.py
@@ -0,0 +1,11 @@
+print('Hola mundo') ##Palabra reservada
+
+print("Hola" + ' Fede')
+print("Me llamo \"federico\"")
+
+print("Esta es una lìnea \nEsta es otra línea")
+print("\tEsta es otra lìnea")
+
+p=input("Ingrese un nùmero: ")
+
+print("EL nùmero que ingresó fue: "+p)
