7db01017babf6393518263d71160bae7f1466adb
GsusMR <jmoreno91@alumnos.uaq.mx>
2025-09-24 21:17:11 -0600

    Created day2 file

---
+++ Dia_2/Variables.py
@@ -0,0 +1,12 @@
+nombre = "Jesus"
+apellido ="Moreno"
+
+nombre_Completo = nombre + " " + apellido
+print(nombre_Completo)
+
+edad = 40
+edad2=12
+print(edad + edad2)
+
+nombre = input("Dime tu nombre ")
+print("Tu nombre es: "+ nombre)
